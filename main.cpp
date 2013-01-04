b461b55c6a5e0b1bb159a20e6c20617ce66b5056
Shaishav Siddhpuria <shaishavps@gmail.com>
2013-01-04 02:11:31 -0500

    Rough, but working implementation of the PR alg.

--- main.cpp
+++ main.cpp
@@ -6,20 +6,27 @@
 int
 main( void )
 {
-    std::cout << PageRank::instance().alpha() << std::endl;
-    PageRank::instance().setAlpha( 0.35 );
-    std::cout << PageRank::instance().alpha() << std::endl;
-
+// keeping track of the list of websites
     std::vector<std::string> webList;
-    webList.push_back ( "google.com" );
-    webList.push_back ( "nba.com" );
-    webList.push_back ( "facebook.com" );
+    webList.push_back ( "websiteOne.com" );
+    webList.push_back ( "websiteTwo.com" );
+    webList.push_back ( "websiteThree.com" );
 
+    // pass the list to singleton, it doesn't make a new copy
     PageRank::instance().setList( webList );
+
+    // perform the necessary computation to determine the rank
+    // currently, this list is not used; a placeholder connectivity
+    // matrix is used until a way to generate the matrix based on
+    // the list is implemented
     PageRank::instance().compute();
+
+    // access the computed list
+    // not really the best implementation, but fine for now
     std::vector<std::string> newList;
     newList = PageRank::instance().computedList();
 
+    // print the list
     std::vector<std::string>::iterator it;
     
     for( it = newList.begin(); it != newList.end(); ++it ) {

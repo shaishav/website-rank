42cd7448ad1d1babe3a411eb6b6e08ba1023e8dc
Shaishav Siddhpuria <shaishavps@gmail.com>
2013-01-04 02:02:23 -0500

    Implement compute algorithm, pending cleanup.

--- PageRank.cpp
+++ PageRank.cpp
@@ -1,5 +1,6 @@
 #include "PageRank.h"
 #include <assert.h>
+#include <armadillo>
 
 /*
  * PageRank class implementatino
@@ -45,7 +46,89 @@ setList( std::vector<std::string>& websites )
 void PageRank::
 compute()
 {
-    m_compList = m_origList;
+    // let's take a sample connectivity matrix for the time being
+    // until there's an implementation for a web crawler in place to generate
+    // this thing
+    arma::mat connectivityMatrix;
+    connectivityMatrix << 1 << 0 << 1 << arma::endr
+        << 0 << 1 << 0 << arma::endr
+        << 1 << 1 << 1 << arma::endr;
+
+    std::cout << "alpha:\n" << m_alpha << std::endl;
+    std::cout << "connectivity matrix:\n" << connectivityMatrix << std::endl;
+
+    arma::mat degMatrix = arma::sum( connectivityMatrix, 0 );
+    
+    std::cout << "degree matrix:\n" << degMatrix << std::endl;
+
+    // need to calculate matrix P, where P = GD ( G = connectivity matrix, D = diagonal mat )
+    int R = std::max( degMatrix.n_rows, degMatrix.n_cols );
+    arma::mat e( R, 1 ); 
+    e.ones();
+
+    std::cout << "e vector:\n" << e << std::endl;
+
+
+    arma::mat p_zero = e / R;
+    arma::mat p_old = p_zero;
+    std::cout << "p_zero matrix:\n" << p_zero << std::endl;
+    std::cout << "p_old matrix:\n" << p_old << std::endl;
+
+    arma::mat d_temp = degMatrix;
+
+    for( unsigned int i = 0; i < d_temp.n_elem; ++i ) {
+        d_temp( i ) = ( degMatrix( i ) == 0 );
+    }
+
+    arma::mat d = d_temp.st();
+    std::cout << "d column vector:\n" << d << std::endl;
+
+    for( unsigned int i = 0; i < degMatrix.n_elem; ++i ) {
+        if( degMatrix(i) == 0 ) {
+            degMatrix(i) = 1;
+        }
+    }
+
+    // solve for D
+    arma::mat D = degMatrix;
+    arma::mat D_ones = D;
+    D_ones.ones();
+    D = D_ones / D;
+
+
+    // main diagonal
+    D = arma::diagmat( D );
+
+    std::cout << "D matrix:\n" << D << std::endl;
+
+    // tolerance level
+    float tol = 1.0e-8;
+
+    // calculate matrix P
+    arma::mat P = connectivityMatrix * D;
+
+
+    int numIterations = 0;
+
+    arma::mat p_new;
+
+    while( true ) {
+        p_new = m_alpha * ( P * p_old + e * ( ( d.t() * p_old ) / R ) ) + ( 1 - m_alpha ) * e/R;  
+
+        numIterations++;
+
+        if( norm( p_new-p_old, "inf" ) < tol ) {
+            break;
+        }
+
+        p_old = p_new;
+     }
+
+    computed_matrix = p_new;
+
+    std::cout << "computed matrix: " << computed_matrix << std::endl;
+
+    return;
 }
 
 std::vector<std::string> PageRank::
